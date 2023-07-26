3b3901fc2e85107d5ca1d5355d20cb5061479614
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-07-26 13:54:59 -0400

    improved frontentd

--- auctions/static/auctions/index.js
+++ auctions/static/auctions/index.js
@@ -15,13 +15,13 @@ const getCookie = (name) => {
 };
 
 const apiCall = async (element, pk, main) => {
-  /** Make an API call with the native fetch API. 
-   * 
+  /** Make an API call with the native fetch API.
+   *
    * element: the button ui element
    * pk: primary key of listing.
    * main: the <main> tag. The message returned from API is prepended to main.
-  */
-  const baseURL = `watchlist/${pk}`
+   */
+  const baseURL = `watchlist/${pk}`;
   const csrfToken = getCookie("csrftoken");
   const isAdd = element.dataset.action == "add" ? true : false;
   const method = isAdd ? "POST" : "DELETE";
@@ -31,23 +31,24 @@ const apiCall = async (element, pk, main) => {
     mode: "same-origin",
   };
   return fetch(baseURL, requestParams)
-    .then((response) => response.json()) 
-      .then((data) => {
-          messageUIAction(main, data.message, data.tags)
-      }).catch((err) => {
-          console.log(err);
-      }) 
+    .then((response) => response.json())
+    .then((data) => {
+      messageUIAction(main, data.message, data.tags);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const closeButtonAPICall = async (element, slug, main) => {
-  /** Make an API call to close a listing with the native fetch API. 
-   * 
+  /** Make an API call to close a listing with the native fetch API.
+   *
    * element: the button ui element
    * slug: slug of listing.
    * main: the <main> tag. The message returned from API is prepended to main.
-  */
+   */
   // The close URL is a part of the ListingView, so the baseURL contains the slug
-  const baseURL = `/${slug}`
+  const baseURL = `/${slug}`;
   const csrfToken = getCookie("csrftoken");
   const method = "PATCH";
   const requestParams = {
@@ -56,16 +57,14 @@ const closeButtonAPICall = async (element, slug, main) => {
     mode: "same-origin",
   };
   return fetch(baseURL, requestParams)
-    .then((response) => {
-      console.log(response);
-      return response.json()}) 
-      .then((data) => {
-        console.log(data);
-          messageUIAction(main, data.message, data.tags);
-          bidCardBodyUIAction(data);
-      }).catch((err) => {
-          console.log(err);
-      }) 
+    .then((response) => response.json())
+    .then((data) => {
+      messageUIAction(main, data.message, data.tags);
+      bidCardBodyUIAction(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const buttonUIAction = (element) => {
@@ -96,7 +95,7 @@ const badgeUIAction = (badge, action) => {
 };
 
 const messageUIAction = (main, message, tags = "info") => {
-/** Add an alert message to the UI **/
+  /** Add an alert message to the UI **/
 
   // Remove old messages if there are any
   const oldMessages = document.querySelector(".messages");
@@ -124,40 +123,38 @@ const bidCardBodyUIAction = (listing) => {
   bidCardBody.innerHTML = "";
 
   // Create 2 card-body elements and append them to .bid-card-body
-  const cardBody1 = document.createElement("div")
-  const cardBody2 = document.createElement("div")
-  cardBody1.classList.add("card-body")
-  cardBody2.classList.add("card-body")
-  
+  const cardBody1 = document.createElement("div");
+  const cardBody2 = document.createElement("div");
+  cardBody1.classList.add("card-body");
+  cardBody2.classList.add("card-body");
 
-  const h5 = document.createElement("h5")
-  h5.classList.add("card-title","text-center")
-  h5.innerText = "Bid"
+  const h5 = document.createElement("h5");
+  h5.classList.add("card-title", "text-center");
+  h5.innerText = "Bid";
 
-  const divM0 = document.createElement("div")
-  divM0.classList.add("m-0")
+  const divM0 = document.createElement("div");
+  divM0.classList.add("m-0");
 
   const cardBody1P = document.createElement("p");
-  cardBody1P.innerText = "The auction is over."
+  cardBody1P.innerText = "The auction is over.";
 
   const cardBody1PWinner = document.createElement("p");
-  cardBody1P.innerText = `${ listing.winner } has won the auction.`
+  cardBody1PWinner.innerText = `${listing.winner} has won the auction.`;
 
   const cardBody2P = document.createElement("p");
-  cardBody2P.innerText = "This auction has ended."
+  cardBody2P.innerText = "This auction has ended.";
 
   // Append children
-  divM0.appendChild(cardBody1P)
-  divM0.appendChild(cardBody1PWinner)
-  
+  divM0.appendChild(cardBody1P);
+  divM0.appendChild(cardBody1PWinner);
   cardBody1.appendChild(h5);
-  cardBody1.appendChild(divM0)
+  cardBody1.appendChild(divM0);
 
   cardBody2.appendChild(cardBody2P);
 
-  bidCardBody.appendChild(cardBody1)
-  bidCardBody.appendChild(cardBody2)
-}
+  bidCardBody.appendChild(cardBody1);
+  bidCardBody.appendChild(cardBody2);
+};
 
 window.addEventListener("DOMContentLoaded", () => {
   const watchlistButton = document.querySelector("#watchlist-button");
@@ -171,7 +168,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const action = element.dataset.action;
     const pk = element.dataset.pk;
 
-    apiCall(element, pk, main)
+    apiCall(element, pk, main);
     badgeUIAction(watchlistLinkBadge, action);
     buttonUIAction(element);
   };
@@ -180,8 +177,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const element = event.target;
     const slug = element.dataset.slug;
     closeButtonAPICall(element, slug, main);
-
-  }
+  };
 
   /** Event listeners */
   if (watchlistButton)
@@ -189,5 +185,4 @@ window.addEventListener("DOMContentLoaded", () => {
 
   if (closeButton)
     closeButton.addEventListener("click", closeButtonClickHandler);
-
 });
